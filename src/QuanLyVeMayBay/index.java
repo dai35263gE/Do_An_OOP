014037512f30cf168d8914e462da58298b676f1d
ThanhLongSGU <long17012006@gmail.com>
2025-10-30 20:54:57 +0700

    Testgit

--- src/QuanLyVeMayBay/index.java
+++ src/QuanLyVeMayBay/index.java
@@ -1,3 +1,6 @@
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
-}
+public class index {
+        public static void main(String[] args) {
+        System.out.println("Welcome to the Flight Ticket Management System");
+        // Additional code for managing flight tickets will go here
+    }
+}  
